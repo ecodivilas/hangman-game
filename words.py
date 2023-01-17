44cccaf5df4dc16b4e8a202d56f9811cc148c2a6
Eleazar Odivilas <eleazar.odivilas01@gmail.com>
2023-01-18 03:23:03 +0800

    Adding More Words in words.py

--- words.py
+++ words.py
@@ -1,5 +1,11 @@
 words = [
-    "eggyolk","eggnog","eggshell",
-    "guitar","code","programming",
-    "youtube","biking","karaoke"
-    ]     # words were hardcoded. Can be dynamic
+         "eggyolk", "eggshell", "guitar", "code", "programming",
+         "youtube", "biking", "karaoke", "frying pan", "strumming",
+         "chords", "tablature", "javascript", "algorithm", "streaming",
+         "handlebars", "singing", "microphone","piano","keyboard",
+         "violin","drums","bass","amplifier","composer","songwriter",
+         "performance","recording","video","lyrics","melody","harmony",
+         "rhythm","beat","tempo","tuning","pedal","conductor","orchestra",
+         "band","DJ","producer","audio","recorder","mixer","equalizer",
+         "compressor","reverb","delay","studio","sound","engineer",
+         "headphones","speakers","amp","effects"]
